aa187e0ef30550ef0c9c54a1db1cafafa249697f
LucasVerissimo7 <48653807+LucasVerissimo7@users.noreply.github.com>
2022-08-24 11:24:57 -0300

    Update script.js

--- Swap/script.js
+++ Swap/script.js
@@ -34,7 +34,7 @@ const contagemRegressiva = (tempo) => {
 
 const tempoRestante = () => {
     // 1 de janeiro de 1970
-    const dataEvento = new Date ('2022-08-29 20:00:00');
+    const dataEvento = new Date ('2022/08/29 20:00:00');
     const hoje = Date.now();
     return Math.floor((dataEvento - hoje) / 1000);
 }
